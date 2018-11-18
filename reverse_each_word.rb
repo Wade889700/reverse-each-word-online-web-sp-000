77dd07457dabed71cb612918245f7bb56edeaecb
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 20:36:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(words)
   words = words.split(" ")
-  binding.pry
   words = words.join(",")
   words = words.reverse
   words = words.split(" ")
@@ -11,6 +10,3 @@ def reverse_each_word(words)
   end
   return my_string.split(",")
 end
-
-expected: "iH ,niaga tsuj gnikam erus s'ti !desrever"
-      got: "!desrever,s'ti,erus,gnikam,tsuj,,niaga,iH"
