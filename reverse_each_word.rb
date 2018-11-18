248d3e7e3bee9f2ab3f01ff3407ebb41c7fd8f56
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 20:44:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -13,7 +13,6 @@ def reverse_each_word(words)
   my_array = []
   new_words.each do|word|
     my_array << word.reverse
-    return my_array
   end
   my_string = my_array.join(" ")
   return my_string
