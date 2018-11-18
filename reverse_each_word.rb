a56682ea4eab591ab1f91ec3315377c2560de6ab
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 20:42:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,13 @@
-def reverse_each_word(words)
-  new_words = words.split(" ")
-  my_array = []
-  new_words.each do |word|
-    my_array << word.reverse
-    return my_array
+
+def reverse_each_word_with_each(string)
+  # returns that same sentence with each word reversed in place.
+  #First solve it using .each Then write the same method using .collect to see the difference.
+  #turn the string into an array
+  #.reverse reverses an array
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
   end
-  my_string = my_array.join(" ")
-  return my_string
+  return_array.join(" ")
 end
