2adef51f0eebc34f5da41174ed1301bd0f77b176
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 20:27:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(words)
   words = words.split(" ")
   words = words.join(",")
   words = words.reverse
-  words = words.split(",")
+  words = words.split(" ")
   my_string = ""
   words.each do |word|
     my_string << word
