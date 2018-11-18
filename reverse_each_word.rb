acf17faabd08e2e13b4b2fa0221ccd57396dab12
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 20:31:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,8 +8,5 @@ def reverse_each_word(words)
     my_string << word
     return my_string
   end
-  my_array = my_string.split(",")
-  my_array_2 = my_array.reverse
-  my_string_2 = my_array.join(" ")
-  return my_string_2
+  return my_string
 end
